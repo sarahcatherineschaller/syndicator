d4af94cc11988bfbb86fba8417f4622809a5e27e
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-01 22:36:40 -0400

    add code for eventbrite submission

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,11 +1,29 @@
+require 'mechanize'
+
 require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '5h' do 
+scheduler.every '1h' do 
 	Event.all do |event|
 		if event.new_record?
-			#syndicate new event to websites
+
+			#Eventbrite
+			agent = Mechanize.new
+			page = agent.get("http://www.eventbrite.com/l/post-events/")
+			form = page.forms[0]
+			eventbrite_event = page.form_with(page.css('#event_form'))
+			eventbrite_event['Event Title'] = event.name 
+			eventbrite_event['Location'] = event.venue 
+			eventbrite_event['Starts'] = event.date 
+			eventbrite_event['Event Description'] = event.description 
+			submit_event = page.link_with(:dom_class => "btn btn--epic submit-for-live")
+			page = submit_event.click 
+
+
+			
+
+
 		end 
 	end
 end
